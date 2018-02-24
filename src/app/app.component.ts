864e4cfe3203c00b10f691be30b60c1e155b5467
daniel0609 <danielgrunwald@o2.pl>
2018-02-24 16:08:00 +0100

    git init

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angulara';
+  pi = Math.PI;
+  date = new Date();
+  dog = new Dog('Reksio', 4);
+
+  days = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela'];
+  dogs = new Array<Dog>();
+  constructor() {
+    this.dogs.push(
+      new Dog('Reksio', 4),
+      new Dog('Łatek', 3),
+      new Dog('Maksiu', 5),
+      new Dog('Łapek', 6),
+      new Dog('Pluto', 8));
+  }
+}
+class Dog {
+  constructor(public name: string, public age: number) { }
+}
