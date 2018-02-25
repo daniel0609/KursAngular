2c23431a871e4c71adfa810aa7d63f948d30ac8e
daniel0609 <danielgrunwald@o2.pl>
2018-02-25 19:07:40 +0100

    done with 2 Way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  newTask: string;
+  noTask = false;
+  taskNum = 0;
+  sameTask = false;
+
+  tasksList: Array<string> = [];
+  // tasksDone: Array<string> = [];
+  tasksDone = new Array<string>();
+  add() {
+    this.sameTask = false;
+    console.log(this.tasksList.findIndex(e => e === this.newTask));
+    if (this.tasksList.findIndex(e => e === this.newTask) < 0) {
+      this.sameTask = false;
+      this.tasksList.push(this.newTask);
+    } else {
+      this.sameTask = true;
+    }
+
+    this.newTask = '';
+  }
+  done(task: string) {
+    this.tasksDone.push(task);
+    this.remove(task);
+  }
+  remove(task: string) {
+
+    this.tasksList = this.tasksList.filter(e => e !== task);
+    console.log(this.taskNum);
+  }
+  removeDone(task: string) {
+    this.tasksDone = this.tasksDone.filter(e => e !== task);
+  }
 }
