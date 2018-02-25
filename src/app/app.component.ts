131b0054b87a6e6691e3c88c54db2253ecdb9d6f
daniel0609 <danielgrunwald@o2.pl>
2018-02-25 11:22:21 +0100

    all bindings done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  inputText = 'Pole Textowe';
+
+  inputText = 'Tekst';
+  inputText2 = '';
+  maxLen = 5;
+  colorClass = 'color';
+  logoUrl = '';
+
+  profession = 'Programista';
+  skill = 'Angulara';
+
+  profession2 = '';
+  skill2 = '';
+
+  isDisable = true;
+  isDisable2 = true;
+
   inputText2Way = '';
   button = true;
   showClick = '';
 
+  constructor() {
+    setTimeout(() => { this.isDisable = false; }, 3000);
+  }
+
+  change() {
+    this.inputText = 'Zamiana tekstu';
+    this.maxLen = 10;
+    this.colorClass = 'color2';
+    this.logoUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
+  }
+
   click(event) {
     this.showClick = 'Przycisk aktywny!';
     this.button = false;
+  }
+
+  onClick(event) {
+    console.log(event);
+  }
+
+  onFocus() {
+    this.colorClass = 'color2';
+  }
+
+  onMouseMove(event) {
+    console.log('x: ' + event.clientX + 'y: ' + event.clientY);
+  }
+
+  onPaste() {
+    this.inputText2 = 'Nie wklejaj, wpisz!';
+  }
+  change2() {
+    this.isDisable2 = false;
+  }
+  saveProff(event) {
+    this.profession = event.target.value;
+  }
+  saveSkill(event) {
+    this.skill = event.target.value;
   }
 }
